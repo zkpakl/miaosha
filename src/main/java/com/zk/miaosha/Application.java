05cdca165ef6f5b954b7f2eb38e62ce0bf7a6dd8
zkpakl <zk_pakl@163.com>
2021-03-30 12:03:43 +0800

    毕设第一次提交

---
+++ src/main/java/com/zk/miaosha/Application.java
@@ -0,0 +1,36 @@
+package com.zk.miaosha;
+
+import com.zk.miaosha.dao.UserDOMapper;
+import com.zk.miaosha.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication(scanBasePackages = {"com.zk.miaosha"})
+@MapperScan("com.zk.miaosha.dao")
+@RestController
+public class Application {
+    @Autowired
+    UserDOMapper userDOMapper;
+
+
+    @RequestMapping("/")
+    public String home(){
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if (userDO == null){
+            return "用户对象不存在";
+        }
+        else {
+            return userDO.getName();
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
